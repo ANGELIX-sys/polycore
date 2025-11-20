c02b89d425bec36a7ff1dd131905fd0d1ae24433
ANGELIX System <45694413+ANGELIX-sys@users.noreply.github.com>
2025-11-20 08:59:09 -0600

    add nicks stuff
    whose nick??

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Polycore Development Flake (For Use By NEOGULISOFT)";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    devShells.${system}.default =
+      pkgs.mkShell
+        {
+          buildInputs = [
+            pkgs.godot
+          ];
+
+          shellHook = ''
+            godot -e
+          '';
+        };
+  };
+}
